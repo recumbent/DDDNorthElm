336990d9f892086e524df66d58a7f2db53fef720
James Murphy <james@recumbent.co.uk>
2017-10-10 21:11:29 +0100

    WIP - Base without comments

--- app.elm
+++ app.elm
@@ -1,89 +1,79 @@
 module Main exposing (..)
 
 -- Import the things we might need
-import Html exposing (Html, h1, h2, div, text, input)
-import Html.Attributes exposing (placeholder)
-import Html.Events exposing (onInput)
 
--- import Html exposing (Html, Attribute, h1, h2, div, text, input)
--- import Html.Attributes exposing (placeholder, value)
--- import Html.Events exposing (onInput, on, keyCode)
--- import Json.Decode as Json
+import Html exposing (Html, Attribute, h1, h2, div, text, input)
+import Html.Attributes exposing (placeholder, value)
+import Html.Events exposing (onInput, on, keyCode)
+import Json.Decode as Json
 
 
 -- MODEL
 
 
 type alias Model =
-    { name : String 
-    -- inputText : String
-    -- , name : String
+    { inputText : String
+    , name : String
     }
 
 
 model : Model
 model =
-    -- { inputText = ""
-    -- ,
-    { name = ""
+    { inputText = ""
+    , name = ""
     }
 
 
 
 -- UPDATE
 
-type Msg =
-    ChangeName String
-    -- = ChangeInput String
-    -- | ChangeName
+
+type Msg
+    = ChangeInput String
+    | ChangeName
 
 
-
-update : Msg -> { b | name : a } -> { b | name : String }
--- update : Msg -> { b | name : a, inputText : a } -> { b | inputText : String, name : a }
+update : Msg -> { b | name : a, inputText : a } -> { b | inputText : String, name : a }
 update msg model =
     case msg of
-        ChangeName newName -> { model | name = newName }
-        -- ChangeInput input ->
-        --     { model | inputText = input }
+        ChangeInput input ->
+            { model | inputText = input }
 
-        -- ChangeName ->
-        --     { model | name = model.inputText, inputText = "" }
+        ChangeName ->
+            { model | name = model.inputText, inputText = "" }
 
 
 
 -- Borrowed from https://github.com/evancz/elm-todomvc/blob/master/Todo.elm
 
 
--- onEnter : Msg -> Attribute Msg
--- onEnter msg =
---     let
---         isEnter code =
---             if code == 13 then
---                 Json.succeed msg
---             else
---                 Json.fail "not ENTER"
---     in
---         on "keydown" (Json.andThen isEnter keyCode)
+onEnter : Msg -> Attribute Msg
+onEnter msg =
+    let
+        isEnter code =
+            if code == 13 then
+                Json.succeed msg
+            else
+                Json.fail "not ENTER"
+    in
+        on "keydown" (Json.andThen isEnter keyCode)
 
 
 
 -- VIEW
 
 
-view : { a | name : String } -> Html Msg
--- view : { a | inputText : String, name : String } -> Html Msg
+view : { a | inputText : String, name : String } -> Html Msg
 view model =
     div []
         [ h1 [] [ text "Simple elm application" ]
         , input
-            [ placeholder "Name to greet", onInput ChangeName ] []
-            -- [ placeholder "Name to greet"
-            -- , onInput ChangeInput
-            -- , onEnter ChangeName
-            -- , value model.inputText
-            -- ]
-            -- []
+            [ placeholder "Name to greet"
+            , onInput ChangeInput
+            , onEnter ChangeName
+            , value model.inputText
+            ]
+            []
         , h2 [] [ text ("Hello " ++ model.name ++ "!") ]
         ]
 
